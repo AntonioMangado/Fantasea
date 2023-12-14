0ec9f5328d64c40d87aaea89d147562a4c7c0251
Mangui <antonio.mangado@gmail.com>
2023-12-14 17:33:22 +0100

    ADD/ write axios code

--- client/src/components/Main/SearchComp/SearchForm/SearchForm.jsx
+++ client/src/components/Main/SearchComp/SearchForm/SearchForm.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import axios from 'axios'
 
-const SearchForm = () => {
+const SearchForm = ({updateList}) => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const query = e.target.book.value;
+    let res = await axios.get(`https://fantasea.onrender.com//api/books/${query}`)
+    let books = res.data
+    console.log(books)
+    // updateList(book)
+  }
+
   return (
   <article id="search-form">
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>What are you looking for?</h3>
       <input type="text" name="book"/>
       <button type="submit">SEARCH</button>
